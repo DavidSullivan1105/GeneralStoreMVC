b1a7947237c973a3617cae5c4e640ca91484ea45
David Sullivan <dcsulliv@gmail.com>
2022-10-05 17:43:14 -0400

    initial commit

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,56 @@
+﻿using System.Linq;
+using GeneralStoreMVC.Data;
+using GeneralStoreMVC.Models.Customer;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GeneralStoreMVC.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private readonly GeneralStoreDBContext _ctx;
+
+        public CustomerController (GeneralStoreDBContext ctx)
+        {
+            _ctx = ctx;
+        }
+        public IActionResult Index()
+        {
+            var customers = _ctx.Customers.Select(customer => new CustomerIndexModel
+            {
+                Id = customer.Id,
+                Name = customer.Name,
+                Email = customer.Email,
+
+            });
+            return View(customers);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create (CustomerCreateModel model)
+        {
+            if(ModelState.IsValid)
+            {
+                TempData["ErrorMsg"] = "Model State is Invalid";
+                return View(model);
+            }
+            _ctx.Customers.Add(new Customer
+            {
+                Name = model.Name,
+                Email = model.Email,
+
+            });
+            if(_ctx.SaveChanges() == 1)
+            {
+                return Redirect("/Customer");
+            }
+            TempData["ErrorMsg"] = "Unable to save to the database. Please try again later.";
+            return View(model);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+using GeneralStoreMVC.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("GSLocal");
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<GeneralStoreDBContext>(options => options.UseSqlServer(connectionString));
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
